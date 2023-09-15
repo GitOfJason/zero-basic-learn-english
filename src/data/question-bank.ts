bd1a8772a18ca4d688596c694f975f20cac3a92f
jason <409859374@qq.com>
2023-09-15 21:54:13 +0800

    fix: 添加一个lesson

--- src/data/question-bank.ts
+++ src/data/question-bank.ts
@@ -3348,12 +3348,166 @@ export const questionBank: QuestionGroup[] = [
   {
     lessonId: "41",
     title: "Lesson41 形式主语it的用法",
-    questions: []
+    questions: [
+      {
+        question:"每天努力学习是好的。",
+        answer: "It is good to study hard every day."
+      },
+      {
+        question:"每天喝酒是不好的。",
+        answer: "It is bad to drink every day."
+      },
+      {
+        question:"每天抽烟是不好的。",
+        answer: "It is bad to smoke every day."
+      },
+      {
+        question:"对于我来说每天去书店是快乐的。",
+        answer: "It is happy for me to go to the bookstore every day."
+      },
+      {
+        question:"对你爸爸来说每天看电视是快乐的。",
+        answer: "It is happy for your father to watch TV every day."
+      },
+      {
+        question:"对于她来说和杰森游泳是快乐的。",
+        answer: "It is happy for her to swim with Jason."
+      },
+      {
+        question:"如果你想学得好，每天努力学习是非常重要的。",
+        answer: "If you want to learn well, it is very important to study hard every day."
+      },
+      {
+        question:"如果你想跑得快，每天练习（practice）是非常重要的。",
+        answer: "If you want to run quickly, it is very important to practice every day."
+      },
+    ]
   },
   {
     lessonId: "42",
     title: "Lesson42 常跟时间搭配的介词at/on/in",
-    questions: []
+    questions: [
+      {
+        question:"几点了？",
+        answer: "What time is it?"
+      },
+      {
+        question:"现在是8点了。",
+        answer: "It is eight o'clock now."
+      },
+      {
+        question:"现在是8点了吗？",
+        answer: "Is it eight o'clock now?"
+      },
+      {
+        question:"是的，现在是8点了。",
+        answer: "Yes, it is eight o'clock now."
+      },
+      {
+        question:"为什么你非常生气？",
+        answer: "Why are you very angry?"
+      },
+      {
+        question:"我很生气是因为我的时钟是坏的（broken）。",
+        answer: "I am very angry because my clock is broken."
+      },
+      {
+        question:"在9点钟我有一个重要的考试，所以我必须吃快点",
+        answer: "I have important test at nine o'clock, so I have to eat quickly."
+      },
+      {
+        question:"我也必须开得很快如果我想要在9点抵达那里。",
+        answer: "I also have to drive quickly if I want to arrive there at nine o'clock."
+      },
+      {
+        question:"何时是玛丽的生日？",
+        answer: "When is Mary's birthday."
+      },
+      {
+        question:"玛丽的生日是在星期六.",
+        answer: "Mary's birthday is on Saturday."
+      },
+      {
+        question:"我想要在星期六庆祝(celebrate)玛丽生日。",
+        answer: "I want to celebrate Mary's birthday on Saturday."
+      },
+      {
+        question:"对我来说庆祝玛丽的生日是非常重要的.",
+        answer: "It is very important for me to celebrate Mary's birthday."
+      },
+      {
+        question:"如果你想要庆祝玛丽的生日，你必须买一个蛋糕。",
+        answer: "If you want to celebrate Mary' birthday, you have to buy a cake."
+      },
+      {
+        question:"星期五我想要在那家蛋糕店买一个蛋糕。",
+        answer: "I want to buy a cake at he cake store on Friday."
+      },
+      {
+        question:"那家蛋糕店在哪里？",
+        answer: "Where is that cake store?"
+      },
+      {
+        question:"那家蛋糕店在书店附近。",
+        answer: "That cake store near the bookstore."
+      },
+      {
+        question:"星期五你想要和我去买个蛋糕吗？",
+        answer: "Do you want to  buy a cake with me on Friday?"
+      },
+      {
+        question:"虽然星期五我有个重要的考试, 但我想要和你买蛋糕。",
+        answer: "Although I have an important test on Friday, I want to buy a cake with you."
+      },
+      {
+        question:"你何时来台湾？",
+        answer: "When do you come to Taiwan?"
+      },
+      {
+        question:"我打算在10月份去台湾。",
+        answer: "I plan to go to Taiwan in October."
+      },
+      {
+        question:"10月份有许多游客（tourists）在台湾。",
+        answer: "There are many tourists in Taiwan in October."
+      },
+      {
+        question:"为什么10月份有许多游客（tourists）在台湾？",
+        answer: "Why are there many tourists in Taiwan in October?"
+      },
+      {
+        question:"因为在10月份台湾天气是非常凉爽的。",
+        answer: "Because it is very cool in Taiwan in October."
+      },
+      {
+        question:"在7月份台湾也有许多美国人吗？",
+        answer: "Are there also many  Americans in Taiwan in July?"
+      },
+      {
+        question:"是的，7月份也有许多美国人在台湾。",
+        answer: "Yes, there are also many Americans in Taiwan in July."
+      },
+      {
+        question:"我喜欢每天上学。",
+        answer: "I like to go to school every day."
+      },
+      {
+        question:"你也喜欢每天上学吗？",
+        answer: "Do you also like to go to school ever day?"
+      },
+      {
+        question:"不，我讨厌每天上学。",
+        answer: "No, I hate to go to school ever day."
+      },
+      {
+        question:"因为学校的那只狗总是喜欢咬（bite）我。",
+        answer: "Because the dog in the school always likes to bite to me."
+      },
+      {
+        question:"如果你想要和玛丽游泳，今天早上你必须完成这个工作。",
+        answer: "If you want to swim with Mary, you have to finish the work this morning."
+      },
+    ]
   },
   {
     lessonId: "43",
